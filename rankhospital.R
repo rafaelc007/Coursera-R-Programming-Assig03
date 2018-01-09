5d1eb33b1ae6262c21373366697fe29ba54e17dd
Rafael Cardoso <raf07ray@gmail.com>
2018-01-09 09:17:23 -0300

    Initial version

---
+++ rankhospital.R
@@ -0,0 +1,6 @@
+
+rankhospital <- function(state, outcome, num){
+	        ## This function return a character vector containing the 
+	        ## name of the hospital with the num-est lowest 30-day death
+	        ## of  heart failure
+}  
